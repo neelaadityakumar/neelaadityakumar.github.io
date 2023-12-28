7eaa89a7554aece2bcfe7f9c3990f7b2b65cbd97
Adity Kumar <neelaadityakumar@gmail.com>
2023-12-28 18:33:52 +0530

    ff

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const production = process.env.NODE_ENV === "production";
-const prodConfig = {
+const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   distDir: "out",
   output: "export",
   basePath: "/github-pages",
 };
-const devConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  distDir: "out",
-};
-let nextConfig = prodConfig;
-if (!production) {
-  nextConfig = { ...devConfig };
-}
 
 module.exports = nextConfig;
