687942647d4db0fd27d5c4bc150e91e0aa814c7a
Adity Kumar <neelaadityakumar@gmail.com>
2023-12-28 17:16:00 +0530

    add

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST='{"userdatapuller/index.tsx -> ../../components/DataPullerProject/Map":{"id":9370,"files":["static/chunks/0b7b90cd.8b90c9ff30094f3f.js","static/css/9f592ed2a2212c51.css","static/chunks/370.c3ac90b9bce5e7a1.js"]}}';
+self.__REACT_LOADABLE_MANIFEST='{"userdatapuller/index.tsx -> ../../components/DataPullerProject/Map":{"id":9370,"files":["static/chunks/0b7b90cd.8b90c9ff30094f3f.js","static/css/8bb5c2adc52acee3.css","static/chunks/370.c3ac90b9bce5e7a1.js"]}}';
