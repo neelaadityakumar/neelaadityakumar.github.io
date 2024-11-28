7e264630294dbb09e04f7d6f35642a7cd742d327
Aditya Kumar <neelaadityakumar@gmail.com>
2024-11-29 02:55:57 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
     distDir: "out",
     output: "export",
     basePath: "/github-pages",
-    assetPrefix: "./blog",
+    assetPrefix: "./",
   };
 }
 
